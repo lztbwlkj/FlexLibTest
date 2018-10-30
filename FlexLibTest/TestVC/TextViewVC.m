e1955c90aa5c53451784f575f40ad78bae7d79bf
lztbwlkj <lztbwlkj@gmail.com>
2018-10-30 09:16:33 +0800

    添加TextView文件

--- FlexLibTest/TestVC/TextViewVC.m
+++ FlexLibTest/TestVC/TextViewVC.m
@@ -8,7 +8,12 @@
 
 #import "TextViewVC.h"
 
-@interface TextViewVC ()
+@interface TextViewVC (){
+    FlexScrollView* scroll;
+    
+    UIView* _imgParent;
+    
+}
 
 @end
 
@@ -21,5 +26,42 @@
 
 }
 
+-(void)removeCell:(UITapGestureRecognizer *)tap{
+    UIView* cell = tap.view;
+    [cell removeFromSuperview];
+    
+    [_imgParent markDirty];
+}
+
+-(void)onAddImage{
+    UITapGestureRecognizer *tap=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(removeCell:)];
+    UIView* cell = [[UIView alloc]init];
+    [cell addGestureRecognizer:tap];
+    
+    [cell enableFlexLayout:YES];
+
+    [cell setLayoutAttrStrings:@[
+                                 @"width",@"20%",
+                                 @"aspectRatio",@"1",
+                                 @"margin",@"2%",
+                                 @"alignItems",@"center",
+                                 @"justifyContent",@"center"
+                                 ]];
+    
+    [cell setViewAttr:@"bgColor" Value:@"#e5e5e5"];
+    [cell setViewAttr:@"borderRadius" Value:@"10"];
+    [_imgParent insertSubview:cell atIndex:0];
+    
+    UILabel* label=[UILabel new];
+    [label enableFlexLayout:YES];
+    [label setViewAttrStrings:@[
+                                @"fontSize",@"16",
+                                @"color",@"red",
+                                @"text",@"删除",
+                                ]];
+    [cell addSubview:label];
+    
+    [_imgParent markDirty];
+}
 
 @end
